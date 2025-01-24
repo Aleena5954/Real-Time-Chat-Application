956f96b2e9e95f0bfa92dd2818db51395dffd693
Aleena <aleenaafaq7@gmail.com>
2025-01-24 20:45:34 +0530

    commit update

--- Real Time Chat Appication/fullstack-chat-app-master/frontend/src/lib/axios.js
+++ Real Time Chat Appication/fullstack-chat-app-master/frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL:"https://real-time-chat-application-a7bs.onrender.com/api",
   withCredentials: true,
 });
