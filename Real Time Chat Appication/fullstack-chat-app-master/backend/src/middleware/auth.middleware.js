76e58a296440932e4a63c0c2f5afbc62c70dd0d2
Aleena <aleenaafaq7@gmail.com>
2025-01-24 23:10:11 +0530

    cokies

--- Real Time Chat Appication/fullstack-chat-app-master/backend/src/middleware/auth.middleware.js
+++ Real Time Chat Appication/fullstack-chat-app-master/backend/src/middleware/auth.middleware.js
@@ -4,6 +4,7 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
+    console.log(res);
     console.log(token);
     res.cookie("jwt", token, {
     httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
